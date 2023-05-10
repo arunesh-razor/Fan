d4fe36f3d381c3806cc13d0315afb17724a754c4
ARUNESH VIKRAM <arunesh-razor@users.noreply.github.com>
2023-05-10 08:13:29 -0500

    Update CeilingFan.java

--- src/main/java/org/example/CeilingFan.java
+++ src/main/java/org/example/CeilingFan.java
@@ -1,29 +1,35 @@
 package org.example;
 
+import org.example.controller.CeilingFanController;
+
 import java.time.LocalDate;
 
-public class Main {
+public class CeilingFan {
     public static void main(String[] args) {
         // Get the singleton instance of the ceiling fan controller
         CeilingFanController controller = CeilingFanController.getInstance();
 
         // Pull the cords to change the state of the ceiling fan
-        controller.setState(LowSpeedState.getInstance());
+        controller.speedUp();
         controller.toggleDirection();
-        controller.setState(MediumSpeedState.getInstance());
+        controller.speedUp();
         controller.toggleDirection();
-        controller.setState(HighSpeedState.getInstance());
-        controller.setState(OffState.getInstance());
-        controller.setState(HighSpeedState.getInstance());
+        controller.speedUp();
+        controller.speedUp();
+        controller.speedUp();
+        controller.toggleDirection();
 
         // Check the current speed and direction of the ceiling fan
         System.out.println("Current speed: " + controller.getCurrentSpeed());
         System.out.println("Direction clockwise? " + controller.getCurrentDirection());
 
-        // Check if the ceiling fan should be off for the holiday
+        // Setting date to holiday
         LocalDate holidayDate = LocalDate.of(2023, 12, 25);
-        if (controller.isOffForHoliday(holidayDate)) {
-            controller.setState(OffState.getInstance());
-        }
+        controller.setDate(holidayDate);
+        controller.speedUp();
+
+        // Check the current speed and direction of the ceiling fan
+        System.out.println("Current speed: " + controller.getCurrentSpeed());
+        System.out.println("Direction clockwise? " + controller.getCurrentDirection());
     }
 }
