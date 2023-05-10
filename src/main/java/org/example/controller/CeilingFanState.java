6a9548b168a502299f91214b11ebd73b9279e217
ARUNESH VIKRAM <arunesh-razor@users.noreply.github.com>
2023-05-10 08:13:16 -0500

    Update CeilingFanState.java

--- src/main/java/org/example/controller/CeilingFanState.java
+++ src/main/java/org/example/controller/CeilingFanState.java
@@ -1,7 +1,8 @@
-package org.example;
+package org.example.controller;
 
 import java.time.LocalDate;
 
+// Ceiling Fan core function skeleton
 public interface CeilingFanState {
     void toggleDirection();
     void speedUp();
